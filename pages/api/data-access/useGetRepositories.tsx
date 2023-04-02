edace9c671a2a7b6da80ce17fbb1e6dbfff1f227
Hamid Farmani <hamidfarmani1@gmail.com>
2023-04-02 21:55:54 +0200

    fix: update the sort of table default value

--- pages/api/data-access/useGetRepositories.tsx
+++ pages/api/data-access/useGetRepositories.tsx
@@ -3,7 +3,7 @@ import { apiClient } from "../../../util/api-client";
 
 async function getRepositories(username) {
   const response = await apiClient.get(
-    `/users/${username}/repos?per_page=100`,
+    `/users/${username}/repos?per_page=100&sort=updated&direction=desc`,
     ""
   );
   return response;
